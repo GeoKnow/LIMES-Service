6c9155b3132eb7518e89b05b9d5a76a28d85970b
Vadim Zaslawski <vadim.zaslawski@server.fake>
2013-12-02 14:50:29 +0100

    Debian dependencies updated.

---
+++ deb-package/debian/limes-service.default.ex
@@ -0,0 +1,10 @@
+# Defaults for limes-service initscript
+# sourced by /etc/init.d/limes-service
+# installed at /etc/default/limes-service by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
---
+++ deb-package/debian/menu.ex
@@ -0,0 +1,2 @@
+?package(limes-service):needs="X11|text|vc|wm" section="Applications/see-menu-manual"\
+  title="limes-service" command="/usr/bin/limes-service"

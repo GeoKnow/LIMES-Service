4451bf6db90e17cd6ebbc7eac89ea9e0dff59fac
jj <jj@TheFarm>
2013-12-12 15:40:27 +0100

    Fixed model output in browser.

--- src/main/java/org/linkeddata/stack/limes/LimesReview.java
+++ src/main/java/org/linkeddata/stack/limes/LimesReview.java
@@ -1,5 +1,6 @@
 package org.linkeddata.stack.limes;
 
+import java.io.ByteArrayOutputStream;
 import java.io.File;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -8,10 +9,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
 import com.hp.hpl.jena.rdf.model.Model;
 import com.hp.hpl.jena.rdf.model.ModelFactory;
 import com.hp.hpl.jena.rdf.model.RDFReader;
-
 import com.google.gson.Gson;
 
 /**
@@ -48,21 +49,26 @@ public class LimesReview extends HttpServlet {
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
     public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	 
     	 response.setHeader("Access-Control-Allow-Origin", "*");
     	
     	 Model mod0 = ModelFactory.createDefaultModel();
     	 Model mod1 = ModelFactory.createDefaultModel();
 
-	   	 RDFReader r0 = mod0.getReader( "N-TRIPLE" );
+	   	 RDFReader r0 = mod0.getReader( "N3" );
 	   	 r0.read( mod0, "file:///"+filePath+"result"+ File.separator + "reviewme.nt" );
-	   	 RDFReader r1 = mod1.getReader( "N-TRIPLE");
+	   	 RDFReader r1 = mod1.getReader( "N3");
 	   	 r1.read(mod1, "file:///"+filePath+"result"+ File.separator + "accepted.nt");
 	   	 
 	   	 //mod0.add(mod1);
 	   	 String[] modArray = new String[2];
-	   	 modArray[0] = mod0.toString();
-	   	 modArray[1] = mod1.toString();
+	   	 
+	   	 ByteArrayOutputStream os1 = new ByteArrayOutputStream();
+	   	 mod0.write(os1);
+	   	 modArray[0] = os1.toString("UTF-8");
+	   	 
+	   	 ByteArrayOutputStream os2 = new ByteArrayOutputStream();
+	   	 mod1.write(os2);
+	   	 modArray[1] = os2.toString();
 	     
 	     Gson gson = new Gson();
 	     String json = gson.toJson(modArray);

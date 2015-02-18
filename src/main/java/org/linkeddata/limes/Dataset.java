efa4a4d6661efb8401d1dbeec5bba268d51c9ccd
Alejandra Garcia-Rojas <alegrm@gmail.com>
2015-02-18 11:18:09 +0100

    last changes

--- src/main/java/org/linkeddata/limes/Dataset.java
+++ src/main/java/org/linkeddata/limes/Dataset.java
@@ -1,5 +1,6 @@
 package org.linkeddata.limes;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.xml.bind.annotation.XmlElement;
@@ -7,8 +8,8 @@ import javax.xml.bind.annotation.XmlType;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-@XmlType(propOrder = { "id", "endpoint", "graph", "var", "pagesize",
-	"restriction", "property", "type" })
+@XmlType(propOrder = { "id", "endpoint", "graph", "var", "pagesize", "restriction", "property",
+        "type" })
 public class Dataset {
 
     private String id;
@@ -20,84 +21,88 @@ public class Dataset {
     private List<String> property;
     private String type;
 
+    public Dataset() {
+        setProperty(new ArrayList<String>());
+    }
+
     @XmlElement(name = "ID")
     @JsonProperty("id")
     public String getId() {
-	return id;
+        return id;
     }
 
     public void setId(String id) {
-	this.id = id;
+        this.id = id;
     }
 
     @XmlElement(name = "ENDPOINT")
     @JsonProperty("endpoint")
     public String getEndpoint() {
-	return endpoint;
+        return endpoint;
     }
 
     public void setEndpoint(String endpoint) {
-	this.endpoint = endpoint;
+        this.endpoint = endpoint;
     }
 
     @XmlElement(name = "GRAPH")
     @JsonProperty("graph")
     public List<String> getGraph() {
-	return graph;
+        return graph;
     }
 
     public void setGraph(List<String> graph) {
-	this.graph = graph;
+        this.graph = graph;
     }
 
     @XmlElement(name = "VAR")
     @JsonProperty("var")
     public String getVar() {
-	return var;
+        return var;
     }
 
     public void setVar(String var) {
-	this.var = var;
+        this.var = var;
     }
 
     @XmlElement(name = "PAGESIZE")
     @JsonProperty("pagesize")
     public String getPagesize() {
-	return pagesize;
+        return pagesize;
     }
 
     public void setPagesize(String pagesize) {
-	this.pagesize = pagesize;
+        this.pagesize = pagesize;
     }
 
     @XmlElement(name = "RESTRICTION")
     @JsonProperty("restriction")
     public String getRestriction() {
-	return restriction;
+        return restriction;
     }
 
     public void setRestriction(String restriction) {
-	this.restriction = restriction;
+        this.restriction = restriction;
     }
 
     @XmlElement(name = "PROPERTY")
     @JsonProperty("property")
     public List<String> getProperty() {
-	return property;
+        return property;
     }
 
     public void setProperty(List<String> property) {
-	this.property = property;
+        this.property = property;
     }
 
     @XmlElement(name = "TYPE")
     @JsonProperty("type")
     public String getType() {
-	return type;
+        return type;
     }
 
     public void setType(String type) {
-	this.type = type;
+        this.type = type;
     }
 
 }
